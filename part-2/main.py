387200542fd28f9464a74314c10f92cb267db736
ivokusi <ilkusijanovic@gmail.com>
2024-10-19 13:35:36 -0500

    Added byte stream response

--- part-2/main.py
+++ part-2/main.py
@@ -247,7 +247,7 @@ st.markdown("---")
 
 st.subheader("Explanation of Prediction")
 
-st.markdown(explanation)
+st.write_stream(explanation)
 
 email = generate_email(avg_probability, input_dict, explanation, selected_customer["Surname"])
 
@@ -255,4 +255,4 @@ st.markdown("---")
 
 st.subheader("Personalized Email")
 
-st.markdown(email)
+st.write_stream(email)
